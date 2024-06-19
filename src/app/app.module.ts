d839627c8cae0d4f3563830077bbc8f2178f0f2f
Javier <javierjr30789@gmail.com>
2024-06-19 11:19:54 -0300

    enrutamiento de los componentes, instalacion de angular material, creacion de los componentes footer y navbar, creacion de los modulos inicio, autentificacion, producto y shared

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SharedModule } from './modules/shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { InicioComponent } from './pages/inicio/inicio.component';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+  
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/modules/inicio/inicio.module.ts
+++ src/app/modules/inicio/inicio.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { InicioRoutingModule } from './inicio-routing.module';
+import { PageComponent } from './page/page.component';
+import { InicioComponent } from './page/inicio/inicio.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageComponent,
+    InicioComponent
+  ],
   imports: [
     CommonModule,
     InicioRoutingModule
